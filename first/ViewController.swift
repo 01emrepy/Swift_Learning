2dad054464bef934166974be9051e33729a83f82
Emre Yıldız <ey56748@gmail.com>
2023-03-11 11:41:52 +0300

    first

--- first/ViewController.swift
+++ first/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextfield: UITextField!
+    
+    @IBOutlet weak var birthdayTextfield:UITextField!
+    
+    @IBOutlet weak var namelabel: UILabel!
+    
+    @IBOutlet weak var birthdaylabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveClicked(_ sender: Any) {
+    }
+    
 }
 
